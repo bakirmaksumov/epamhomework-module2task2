ac397cd4e5b67f6606799613548e233bf2918acb
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-07-12 11:16:33 +0500

    initial

---
+++ WindowsHelloForms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsHelloForms
+{
+    public partial class AppMainForm : Form
+    { 
+        public AppMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string inputTxt=txtMain.Text;
+            if (inputTxt != String.Empty)
+            {
+                MessageBox.Show($"Hello {inputTxt}");
+            }
+            else
+            {
+                MessageBox.Show("textbox is empty");
+            }
+        }
+    }
+}
